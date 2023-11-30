35e4fcf4137b6536dc5caf39ea12055ef420e54a
DustinFader <dustinfader16@gmail.com>
2023-11-29 23:12:55 -0400

    add a bunch of query files with sql queries.

---
+++ 1_queries/Github_Activity.sql
@@ -0,0 +1,3 @@
+SELECT name, email, phone FROM students WHERE github IS NULL AND end_date IS NOT NULL;
+-- i went a little too fast and got this wrong the first time on what was expected. 
+-- Being the end_date didnt come to mind. So basicly 35min for chap and some extra time with filing git stuff.
