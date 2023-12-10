3b545b4a7675cfa19b43d7e8a794c767a64a6baf
DustinFader <dustinfader16@gmail.com>
2023-12-10 19:34:28 -0400

    create a java query that logs to the terminal using sql

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+const argv = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '${argv[0] || 'JUL02'}%'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+  })
+  .catch(err => console.error('querry error', err.stack));
